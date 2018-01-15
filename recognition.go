530fce8749d2b323af40c773a006b802e37deed3
Spotlight <awesomebing1@gmail.com>
2018-01-15 01:01:22 -0600

    Make more go-like and pass on errors

--- recognition.go
+++ recognition.go
@@ -7,11 +7,13 @@ import (
 	"fmt"
 	"crypto/cipher"
 	"crypto/aes"
+	"errors"
 )
 
+func Extract(file []byte, pubk []byte) ([]byte, error) {
 	// The content header is always a certain length, so the file needs to be larger.
 	if 0x140 > len(file) {
-		panic("File seems too small.")
+		return nil, errors.New("file seems too small")
 	}
 
 	contentHeader := ContentHeader{}
@@ -19,39 +21,41 @@ import (
 	headerReadingBuf := bytes.NewBuffer(file[:64])
 	err := binary.Read(headerReadingBuf, binary.BigEndian, &contentHeader)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	if ContentHeaderMagic != string(contentHeader.Magic[:]) {
-		panic("Invalid magic!")
+	// This'll return 0 if they're exactly the same.
+	if bytes.Compare(ContentHeaderMagic, contentHeader.Magic[:]) != 0 {
+		return nil, errors.New("invalid magic")
 	}
 	log.Print("Version detected: " + fmt.Sprint(contentHeader.Version))
 	switch contentHeader.EncryptionType {
 	case 0:
-		panic("I don't support plaintext extraction, but I'm pretty sure you can yourself w/ hex editor. ;P")
+		// there's no encryption to do here. just return 0x140 on
+		return file[0x140:], nil
 		break
 	case 1:
 		break
 	default:
-		panic("Unknown WC24 container version.")
+		return nil, errors.New("unknown encryption type given")
 	}
 
 	// if we're here, we need to decrypt.
 	// let's get this party started!
 	if 0x220 != len(pubk) {
-		panic("pubk seems too small.")
+		return nil, errors.New("pubk seems too small")
 	}
 	pubkHeader := PubkFormat{}
 	pubkReadingBuff := bytes.NewBuffer(pubk)
 	err = binary.Read(pubkReadingBuff, binary.BigEndian, &pubkHeader)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	// Set up decryption using given IV and AES Key
-	block, err := aes.NewCipher(pubkHeader.AES_key[:])
+	block, err := aes.NewCipher(pubkHeader.AESKey[:])
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	decryptedBuf := make([]byte, len(file)-0x140)
@@ -59,5 +63,5 @@ import (
 	stream := cipher.NewOFB(block, contentHeader.IV[:])
 	// Skip into 0x140 as that's where content starts
 	stream.XORKeyStream(decryptedBuf, file[0x140:])
-	return decryptedBuf
+	return decryptedBuf, nil
 }
